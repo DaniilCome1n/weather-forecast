0f60869a7e29240ca1d221f7487789fddaa9ac50
Daniil <daniilkom1n@yandex.ru>
2021-05-14 00:02:31 +0300

    minor changes

---
+++ src/views/main-layout.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="main-wrapper">
+    <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+      <template slot="progress">
+        <v-progress-linear
+          color="deep-purple"
+          height="5"
+          indeterminate
+        ></v-progress-linear>
+      </template>
+      <v-card-title>Cafe Badilico</v-card-title>
+      <v-card-text></v-card-text>
+      <v-divider class="mx-4"></v-divider>
+      <v-card-title>Tonight's availability</v-card-title>
+      <v-card-text></v-card-text>
+      <v-card-actions></v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "main-layout",
+  data() {
+    return {
+      loading: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.main-wrapper {
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  background-image: url("../assets/img/background.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
