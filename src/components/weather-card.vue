c2732bbdc503066fcfad6959500859541ef5b8a6
Daniil <daniilkom1n@yandex.ru>
2021-05-18 00:14:16 +0300

    add weather card component

---
+++ src/components/weather-card.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-card v-if="!currentWeather.isEmpty" class="mx-auto my-2" width="350">
+    <v-card-title>{{ currentWeather.city }}</v-card-title>
+    <div class="main-info">
+      <div class="left">
+        <img
+          :src="require(`@/assets/weather-icons/${currentWeather.weatherIcon}.svg`)"
+          alt="weather-icon"
+        />
+        <span class="left__description">{{ currentWeather.description }}</span>
+      </div>
+      <div class="right">
+        <span class="right__degrees" v-html="`${currentWeather.temp}&degC`"></span>
+      </div>
+    </div>
+    <v-card-text></v-card-text>
+    <v-divider class="mx-4"></v-divider>
+    <v-card-title>Tonight's availability</v-card-title>
+    <v-card-text></v-card-text>
+    <v-card-actions></v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "weather-card",
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters([
+      "currentWeather",
+    ]),
+  },
+  mounted() {
+  },
+};
+</script>
+
+<style scoped lang="less">
+.main-info {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: 15px;
+  .left {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    img {
+      width: 110px;
+    }
+    .left__description::first-letter{
+      text-transform: uppercase;
+    }
+  }
+  .right {
+    margin-top: 11px;
+    .right__degrees{
+      font-size: 60px;
+    }
+  }
+}
+</style>
--- src/views/main-layout.vue
+++ src/views/main-layout.vue
@@ -1,46 +1,48 @@
 <template>
   <div class="main-wrapper">
-    <v-card :loading="loading" class="mx-auto my-12" max-width="374">
-      <template slot="progress">
-        <v-progress-linear
-          color="deep-purple"
-          height="5"
-          indeterminate
-        ></v-progress-linear>
-      </template>
-      <v-card-title>Cafe Badilico</v-card-title>
-      <v-card-text></v-card-text>
-      <v-divider class="mx-4"></v-divider>
-      <v-card-title>Tonight's availability</v-card-title>
-      <v-card-text></v-card-text>
-      <v-card-actions></v-card-actions>
-    </v-card>
+    <div class="search-wrapper">
+      <v-text-field
+        v-model="cityName"
+        label="Найди свой город"
+        append-icon="mdi-cloud-search"
+        outlined
+        shaped
+        @click:append="findWeatherByCity"
+      ></v-text-field>
+    </div>
+    <weatherCard />
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions } from "vuex";
+import weatherCard from "@/components/weather-card";
 
 export default {
+  components: {
+    weatherCard,
+  },
   name: "main-layout",
   data() {
     return {
-      loading: false,
+      cityName: "",
     };
   },
-  mounted() {
-    this.getWeather()
+  async mounted() {
+    await this.getWeather();
   },
-  methods:{
-    ...mapActions([
-      'getWeather'
-    ]),
-  }
+  methods: {
+    ...mapActions(["getWeather"]),
+    findWeatherByCity() {
+      this.getWeather(this.cityName);
+    },
+  },
 };
 </script>
 
-<style scoped>
+<style scoped lang="less">
 .main-wrapper {
+  font-family: 'Montserrat', sans-serif;
   width: 100%;
   height: 100vh;
   position: absolute;
@@ -48,7 +50,12 @@ export default {
   background-size: cover;
   background-repeat: no-repeat;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  .search-wrapper {
+    height: 50px;
+    width: 350px;
+  }
 }
 </style>
