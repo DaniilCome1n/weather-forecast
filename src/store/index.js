249fc2ba362333d1dd4e8dd2f2fd62633c4ad558
Daniil <daniilkom1n@yandex.ru>
2021-05-18 00:13:50 +0300

    add new getters for current weather forecast

--- src/store/index.js
+++ src/store/index.js
@@ -7,12 +7,39 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     apiKey: "3ef8cdde52fcb98a90ec0fc5a0f5f476",
+    weatherByCity: {},
   },
-  mutations: {},
+  mutations: {
+    setWeather(state, weather) {
+      state.weatherByCity = weather;
+    },
+  },
+  getters:{
+    currentWeather: state => {
+      let isForecastEmpty = Object.keys(state.weatherByCity).length === 0;
+      return {
+        isEmpty: isForecastEmpty,
+        city: isForecastEmpty ?  "" : state.weatherByCity.name,
+        weatherIcon: isForecastEmpty ?  "01n" : state.weatherByCity.weather[0].icon,
+        description: isForecastEmpty ?  "" : state.weatherByCity.weather[0].description,
+        temp : isForecastEmpty ?  0 : Math.floor(state.weatherByCity.main.temp)
+      }
+    },
+  },
   actions: {
-    async getWeather() {
-      console.log(this.state.apiKey)
-      await fetch(`${baseURL}?q=London&appid=${this.state.apiKey}`);
+    async getWeather({ commit },cityName) {
+      if (!cityName) {
+        cityName = "Krasnodar";
+      }
+      const response = await fetch(
+        `${baseURL}?q=${cityName}&units=metric&lang=ru&appid=${this.state.apiKey}`
+      );
+      const currentWeather = await response.json();
+      if (currentWeather.cod === "404") {
+        console.log("Город не найден");
+        return;
+      }
+      commit("setWeather", currentWeather);
     },
   },
   modules: {},
